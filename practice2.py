86796d0301b6bfc01fd6e8e29b157f14d49baaf7
emankhattak39 <emankhattak39@gmail.com>
2024-12-11 22:16:25 +0500

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,14 @@
+print("hello world!\nhello world!\nhello world!")
+
+print("hello" + " " + "angela")
+
+
+print("day1 - String manupilation")
+print('String cancatenation is done with "+" sign.')
+print('e.g .print("hello world "+" world)')
+print("new lines can be created with a backslash and n.")
+
+
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
